2134a1fc45e6a3bc1e504bb82ea271573486b3ba
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-04 18:10:30 -0400

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
+#Autor: EdwardRamos
+#Date: 10/04/2016 U.S.A
 
-valor1 = input('Enter o primeiro valor: ')
-valor2 = input('Enter o segundo valor: ')
+valor1 = raw_input("Enter o primeiro valor: ")
+valor2 = raw_input('Enter o segundo valor: ')
 
 if valor1 == valor2:
     print("Valores iguais!!")
-    print("(+) Primeiro valor: %d") % (valor1)
-    print("(+) Segundo valor: %d") % (valor2)
+    print("(+) Primeiro valor: %s") % (str(valor1))
+    print("(+) Segundo valor: %s") % (str(valor2))
 elif valor1 >= valor2:
-    print("(+) Primeiro valor e maior: %d") % (valor1)
+    print("(+) Primeiro valor e maior: %s") % (str(valor1))
 elif valor1 <= valor2:
-    print("(+) Segundo valor e maior: %d") % (valor2)
+    print("(+) Segundo valor e maior: %s") % (str(valor2))
 
 else:
     ("(-) error")
