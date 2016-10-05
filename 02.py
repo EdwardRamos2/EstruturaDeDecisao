32e3514a0aff367c456937854bdfacd863bbae13
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-04 21:13:45 -0400

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#*-*coding: utf-8 *-*
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+
+positivo = input("Insira numero: ")
+
+if positivo >= -0:
+    print ("numero positivo: %d") % (positivo)
+else:
+    print("Numero negativo: %d") % (positivo)
