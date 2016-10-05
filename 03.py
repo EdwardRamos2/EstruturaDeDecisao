bf1ccf990c636750a3ca98b174ba1cdbbce259a6
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-04 22:32:12 -0400

    Update 03.py

--- 03.py
+++ 03.py
@@ -4,7 +4,7 @@
 #Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 #Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
-sexo = input('Digite F Feminino ou M Masculino: ')
+sexo = input('Digite (F Feminino) ou (M Masculino): ')
 print(sexo)
 
 if sexo.upper() == 'M':
