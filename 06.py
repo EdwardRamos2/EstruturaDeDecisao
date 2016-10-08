699455699b746ce0ac479665ef8bbdc4f9e5fe05
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-07 22:19:24 -0400

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+#Date: 10/07/2016 U.S.A
+
+
+#Faça um Programa que leia três números e mostre o maior deles.
+
+
+num1 = int(input('Insira o primeiro numero: '))
+num2 = int(input('Insira o segundo numero: '))
+num3 = int(input('Insira o terceiro numero: '))
+
+
+if num1 > num2 > num3:
+    print('(+) Primeiro numero e o maior: %r' % num1)
+    
+if num1 > num3 > num2:
+    print('(+) Primeiro numero e o maior: %r' % num1)
+    
+if num2 > num1 > num3:
+    print('(+) Segundo numero e o maior: %r' % num2)
+    
+if num2 > num3 > num1:
+    print('(+) Segundo numero e o maior: %r' % num2)
+    
+if num3 > num1 > num2:
+    print('(+) Terceiro numero e o maior: %r' % num3)
+    
+if num3 > num2 > num1:
+    print('(+) Terceiro numero e o maior: %r' % num3)
+    
+if num1 == num2 == num3:        
+    print('(+) Numeros repetidos sao anulados')
