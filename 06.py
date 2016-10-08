82cf413ed9ee7744681f8cb4b3686c4c85429658
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-07 22:20:16 -0400

    Update 06.py

--- 06.py
+++ 06.py
@@ -13,22 +13,22 @@ num3 = int(input('Insira o terceiro numero: '))
 
 
 if num1 > num2 > num3:
-    print('(+) Primeiro numero e o maior: %r' % num1)
+    print('(+) Primeiro numero e o maior: %d' % num1)
     
 if num1 > num3 > num2:
-    print('(+) Primeiro numero e o maior: %r' % num1)
+    print('(+) Primeiro numero e o maior: %d' % num1)
     
 if num2 > num1 > num3:
-    print('(+) Segundo numero e o maior: %r' % num2)
+    print('(+) Segundo numero e o maior: %d' % num2)
     
 if num2 > num3 > num1:
-    print('(+) Segundo numero e o maior: %r' % num2)
+    print('(+) Segundo numero e o maior: %d' % num2)
     
 if num3 > num1 > num2:
-    print('(+) Terceiro numero e o maior: %r' % num3)
+    print('(+) Terceiro numero e o maior: %d' % num3)
     
 if num3 > num2 > num1:
-    print('(+) Terceiro numero e o maior: %r' % num3)
+    print('(+) Terceiro numero e o maior: %d' % num3)
     
 if num1 == num2 == num3:        
     print('(+) Numeros repetidos sao anulados')
