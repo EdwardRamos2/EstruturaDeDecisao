d52f5dd4194105b8c16bcbcd0753947599abde24
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-07 20:46:17 -0400

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+#Date: 10/07/2016 U.S.A
+
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante. 
+
+
+letra = input('Insira uma letra: ')
+letra2 = letra.upper()
+if 'A' in letra2:
+    print('(+) Uma Vogal')
+
+elif 'E' in letra2:
+    print('(+) Uma Vogal')
+
+elif 'I' in letra2:
+    print('(+) Uma Vogal')
+
+elif 'O' in letra2:
+    print('(+) Uma Vogal')
+
+
+elif 'U' in letra2:
+    print('(+) Uma Vogal')
+
+else: 
+    print('(+) Uma Consoante')
